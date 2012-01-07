1c343394dfdeeb7a0d5d53371a87a6179c6e3ff1
Fjölnir Ásgeirsson <me@fyolnish.com>
2012-01-07 18:48:54 +0900

    Added a mechanism for scripts to have a function called on each frame

--- Tranquil/Scripts/startup.lua
+++ Tranquil/Scripts/startup.lua
@@ -1,4 +1,10 @@
-// Called at the end of each frame (beginning of the next)
+-- Called at the end of each frame (beginning of the next)
+_frameCallbacks = {}
 function _frameCallback()
-
+	for i,callback in ipairs(_frameCallbacks) do
+		callback()
+	end
+end
+function everyFrame(aCallback)
+	_frameCallbacks[#_frameCallbacks + 1] = aCallback
 end
