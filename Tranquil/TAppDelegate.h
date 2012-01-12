fbe6c03340b707da9d22f728b2eff154dce8cc04
Fjölnir Ásgeirsson <me@fyolnish.com>
2012-01-13 00:24:36 +0900

    Load a test shader on startup

--- Tranquil/TAppDelegate.h
+++ Tranquil/TAppDelegate.h
@@ -1,2 +1,7 @@
+@class TOpenGLView;
+
+NSOpenGLContext *TGlobalGLContext();
+
 @interface TAppDelegate : NSObject <NSApplicationDelegate>
+@property(readonly) IBOutlet TOpenGLView *glView;
 @end
--- Tranquil/TAppDelegate.m
+++ Tranquil/TAppDelegate.m
@@ -2,15 +2,32 @@
 #import "TMainWindowController.h"
 #import "TScriptContext.h"
 #import "TPluginManager.h"
+#import "TScene.h"
+#import "TShader.h"
+#import "TState.h"
+#import "TOpenGLView.h"
+#import "TGLErrorChecking.h"
+#import "TOpenGLLayer.h"
+
+NSOpenGLContext *_globalGlContext = nil;
+
+NSOpenGLContext *TGlobalGLContext() {
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		_globalGlContext = [[NSOpenGLContext alloc] initWithFormat:[TOpenGLLayer pixelFormat] shareContext:nil];
+	});
+	return _globalGlContext;
+}
 
 @implementation TAppDelegate
+@synthesize glView;
 
 - (void)dealloc
 {
     [super dealloc];
 }
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+- (void)applicationWillFinishLaunching:(NSNotification *)notification
 {
 	NSString *path = [[NSBundle mainBundle] pathForResource:@"startup" ofType:@"lua" inDirectory:@"Scripts"];
 	NSString *bootScript = [NSString stringWithContentsOfFile:path usedEncoding:NULL error:nil];
@@ -20,7 +37,18 @@
 		NSLog(@"Error executing startup script: %@", err);
 		[NSApp terminate:nil];
 	}
-	
+
+	[TGlobalGLContext() makeCurrentContext];
+	TCheckGLError();
+	NSString *fragSrc = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"simple" ofType:@"fsh" inDirectory:@"Shaders"]
+											  usedEncoding:NULL error:NULL];
+	NSString *vertSrc = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"simple" ofType:@"vsh" inDirectory:@"Shaders"]
+											  usedEncoding:NULL error:NULL];
+
+	TShader *shader = [TShader shaderWithName:@"Simple" fragmentShader:fragSrc vertexShader:vertSrc];
+	TCheckGLError();
+	[[TScene globalScene] currentState].shader = shader;
+
 	[[TPluginManager sharedManager] loadAllPlugins];
 }
 
