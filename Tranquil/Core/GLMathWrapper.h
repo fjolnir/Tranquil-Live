efe13d17975fe56efaa4aba9cee9f4d24a45abdb
Fjölnir Ásgeirsson <me@fyolnish.com>
2012-01-28 00:15:58 +0900

    Replaced all accessor calls with direct access

--- Tranquil/Core/GLMathWrapper.h
+++ Tranquil/Core/GLMathWrapper.h
@@ -1,6 +1,6 @@
 #include <GLMath/GLMath.h>
 
-@interface Vector2 : NSObject
+@interface Vector2 : NSObject { @public vec2_t _vec; }
 @property(readwrite, assign) vec2_t vec;
 @property(readwrite, assign) float x, y;
 
@@ -24,7 +24,7 @@
 - (Vector2 *)vec2_scalarDiv:(float)aScalar;
 @end
 
-@interface Vector3 : NSObject
+@interface Vector3 : NSObject { @public vec3_t _vec; }
 @property(readwrite, assign) vec3_t vec;
 @property(readwrite, assign) float x, y, z;
 
@@ -49,7 +49,7 @@
 - (Vector3 *)vec3_scalarDiv:(float)aScalar;
 @end
 
-@interface Vector4 : NSObject
+@interface Vector4 : NSObject { @public vec4_t _vec; }
 @property(readwrite, assign) vec4_t vec;
 @property(readwrite, assign) float x, y, z, w;
 @property(readwrite, assign) float r, g, b, a;
@@ -75,7 +75,7 @@
 - (Vector4 *)vec4_scalarDiv:(float)aScalar;
 @end
 
-@interface Matrix3 : NSObject
+@interface Matrix3 : NSObject { @public mat3_t _mat; }
 @property(readwrite, assign) mat3_t mat;
 + (Matrix3 *)matrixWithMat:(mat3_t)aMat;
 
@@ -86,7 +86,7 @@
 - (float)determinant;
 @end
 
-@interface Matrix4 : NSObject
+@interface Matrix4 : NSObject { @public mat4_t _mat; }
 @property(readwrite, assign) mat4_t mat;
 + (Matrix4 *)matrixWithMat:(mat4_t)aMat;
 + (Matrix4 *)identity;
@@ -115,7 +115,7 @@
 - (Matrix4 *)scaleWithX:(float)x y:(float)y z:(float)z;
 @end
 
-@interface MatrixStack : NSObject
+@interface MatrixStack : NSObject { @public matrix_stack_t *_stack; }
 @property(readwrite, assign) matrix_stack_t *stack;
 @property(readonly) Matrix4 *top, *topAsMat3;
 + (MatrixStack *)stackWithCapacity:(int)aCapacity;
@@ -130,7 +130,7 @@
 - (void)rotateWithAngle:(float)angle x:(float)x y:(float)y z:(float)z;
 @end
 
-@interface Quaternion : NSObject
+@interface Quaternion : NSObject { @public quat_t _quat; }
 @property(readwrite, assign) quat_t quat;
 + (Quaternion *)quaternionWithAngle:(float)aAngle x:(float)aX y:(float)aY z:(float)aZ;
 + (Quaternion *)quaternionWithQuat:(quat_t)aQuat;
--- Tranquil/Core/GLMathWrapper.m
+++ Tranquil/Core/GLMathWrapper.m
@@ -6,12 +6,12 @@
 
 + (Vector2 *)vectorWithX:(float)aX y:(float)aY {
 	Vector2 *out = [[self alloc] init];
-	out.vec = vec2_create(aX, aY);
+	out->_vec = vec2_create(aX, aY);
 	return out;
 }
 + (Vector2 *)vectorWithVec:(vec2_t)aVec {
 	Vector2 *out = [[self alloc] init];
-	out.vec = aVec;
+	out->_vec = aVec;
 	return out;
 }
 
@@ -21,23 +21,23 @@
 - (void)setY:(float)n { _vec.y = n; }
 
 - (Vector2 *)add:(Vector2 *)aOther {
-	return [Vector2 vectorWithVec:vec2_add(_vec, aOther.vec)];
+	return [Vector2 vectorWithVec:vec2_add(_vec, aOther->_vec)];
 }
 - (Vector2 *)sub:(Vector2 *)aOther {
-	return [Vector2 vectorWithVec:vec2_sub(_vec, aOther.vec)];
+	return [Vector2 vectorWithVec:vec2_sub(_vec, aOther->_vec)];
 }
 - (Vector2 *)mul:(Vector2 *)aOther {
-	return [Vector2 vectorWithVec:vec2_mul(_vec, aOther.vec)];
+	return [Vector2 vectorWithVec:vec2_mul(_vec, aOther->_vec)];
 }
 - (Vector2 *)div:(Vector2 *)aOther {
-	return [Vector2 vectorWithVec:vec2_div(_vec, aOther.vec)];
+	return [Vector2 vectorWithVec:vec2_div(_vec, aOther->_vec)];
 }
 
 - (float)dot:(Vector2 *)aOther {
-	return vec2_dot(_vec, aOther.vec);
+	return vec2_dot(_vec, aOther->_vec);
 }
 - (float)distanceToPoint:(Vector2 *)aOther {
-	return vec2_dist(_vec, aOther.vec);
+	return vec2_dist(_vec, aOther->_vec);
 }
 
 - (float)magnitude {
@@ -72,12 +72,12 @@
 
 + (Vector3 *)vectorWithX:(float)aX y:(float)aY z:(float)aZ {
 	Vector3 *out = [[self alloc] init];
-	out.vec = vec3_create(aX, aY, aZ);
+	out->_vec = vec3_create(aX, aY, aZ);
 	return out;
 }
 + (Vector3 *)vectorWithVec:(vec3_t)aVec {
 	Vector3 *out = [[self alloc] init];
-	out.vec = aVec;
+	out->_vec = aVec;
 	return out;
 }
 
@@ -89,26 +89,26 @@
 - (void)setZ:(float)n { _vec.z = n; }
 
 - (Vector3 *)add:(Vector3 *)aOther {
-	return [Vector3 vectorWithVec:vec3_add(_vec, aOther.vec)];
+	return [Vector3 vectorWithVec:vec3_add(_vec, aOther->_vec)];
 }
 - (Vector3 *)sub:(Vector3 *)aOther {
-	return [Vector3 vectorWithVec:vec3_sub(_vec, aOther.vec)];
+	return [Vector3 vectorWithVec:vec3_sub(_vec, aOther->_vec)];
 }
 - (Vector3 *)mul:(Vector3 *)aOther {
-	return [Vector3 vectorWithVec:vec3_mul(_vec, aOther.vec)];
+	return [Vector3 vectorWithVec:vec3_mul(_vec, aOther->_vec)];
 }
 - (Vector3 *)div:(Vector3 *)aOther {
-	return [Vector3 vectorWithVec:vec3_div(_vec, aOther.vec)];
+	return [Vector3 vectorWithVec:vec3_div(_vec, aOther->_vec)];
 }
 - (Vector3 *)cross:(Vector3 *)aOther {
-	return [Vector3 vectorWithVec:vec3_cross(_vec, aOther.vec)];
+	return [Vector3 vectorWithVec:vec3_cross(_vec, aOther->_vec)];
 }
 
 - (float)dot:(Vector3 *)aOther {
-	return vec3_dot(_vec, aOther.vec);
+	return vec3_dot(_vec, aOther->_vec);
 }
 - (float)distanceToPoint:(Vector3 *)aOther {
-	return vec3_dist(_vec, aOther.vec);
+	return vec3_dist(_vec, aOther->_vec);
 }
 
 - (float)magnitude {
@@ -142,12 +142,12 @@
 
 + (Vector4 *)vectorWithX:(float)aX y:(float)aY z:(float)aZ w:(float)aW {
 	Vector4 *out = [[self alloc] init];
-	out.vec = vec4_create(aX, aY, aZ, aW);
+	out->_vec = vec4_create(aX, aY, aZ, aW);
 	return out;
 }
 + (Vector4 *)vectorWithVec:(vec4_t)aVec {
 	Vector4 *out = [[self alloc] init];
-	out.vec = aVec;
+	out->_vec = aVec;
 	return out;
 }
 
@@ -166,26 +166,26 @@
 - (void)setW:(float)n { _vec.w = n; }
 
 - (Vector4 *)add:(Vector4 *)aOther {
-	return [Vector4 vectorWithVec:vec4_add(_vec, aOther.vec)];
+	return [Vector4 vectorWithVec:vec4_add(_vec, aOther->_vec)];
 }
 - (Vector4 *)sub:(Vector4 *)aOther {
-	return [Vector4 vectorWithVec:vec4_sub(_vec, aOther.vec)];
+	return [Vector4 vectorWithVec:vec4_sub(_vec, aOther->_vec)];
 }
 - (Vector4 *)mul:(Vector4 *)aOther {
-	return [Vector4 vectorWithVec:vec4_mul(_vec, aOther.vec)];
+	return [Vector4 vectorWithVec:vec4_mul(_vec, aOther->_vec)];
 }
 - (Vector4 *)div:(Vector4 *)aOther {
-	return [Vector4 vectorWithVec:vec4_div(_vec, aOther.vec)];
+	return [Vector4 vectorWithVec:vec4_div(_vec, aOther->_vec)];
 }
 - (Vector4 *)cross:(Vector4 *)aOther {
-	return [Vector4 vectorWithVec:vec4_cross(_vec, aOther.vec)];
+	return [Vector4 vectorWithVec:vec4_cross(_vec, aOther->_vec)];
 }
 
 - (float)dot:(Vector4 *)aOther {
-	return vec4_dot(_vec, aOther.vec);
+	return vec4_dot(_vec, aOther->_vec);
 }
 - (float)distanceToPoint:(Vector4 *)aOther {
-	return vec4_dist(_vec, aOther.vec);
+	return vec4_dist(_vec, aOther->_vec);
 }
 
 - (float)magnitude {
@@ -218,15 +218,15 @@
 
 + (Matrix3 *)matrixWithMat:(mat3_t)aMat {
 	Matrix3 *out = [[self alloc] init];
-	out.mat = aMat;
+	out->_mat = aMat;
 	return out;
 }
 
 - (Matrix3 *)mul:(Matrix3 *)aOther {
-	return [Matrix3 matrixWithMat:mat3_mul(_mat, aOther.mat)];
+	return [Matrix3 matrixWithMat:mat3_mul(_mat, aOther->_mat)];
 }
 - (Vector3 *)mulVector:(Vector3 *)aVector {
-	return [Vector3 vectorWithVec:vec3_mul_mat3(aVector.vec, _mat)];
+	return [Vector3 vectorWithVec:vec3_mul_mat3(aVector->_vec, _mat)];
 }
 - (Matrix3 *)inverse {
 	return [Matrix3 matrixWithMat:mat3_inverse(_mat, NULL)];
@@ -244,7 +244,7 @@
 
 + (Matrix4 *)matrixWithMat:(mat4_t)aMat {
 	Matrix4 *out = [[self alloc] init];
-	out.mat = aMat;
+	out->_mat = aMat;
 	return out;
 }
 
@@ -258,54 +258,54 @@
 // Viewing matrices
 + (Matrix4 *)perspectiveMatrixWithFov:(float)aFov aspectRatio:(float)aAspect zNear:(float)aZNear zFar:(float)aZFar {
 	Matrix4 *out = [[self alloc] init];
-	out.mat = mat4_perspective(aFov, aAspect, aZNear, aZFar);
+	out->_mat = mat4_perspective(aFov, aAspect, aZNear, aZFar);
 	return out;
 }
 + (Matrix4 *)frustumWithLeft:(float)aLeft right:(float)aRight bottom:(float)aBottom top:(float)aTop near:(float)aNear far:(float)aFar {
 	Matrix4 *out = [[self alloc] init];
-	out.mat = mat4_frustum(aLeft, aRight, aBottom, aTop, aNear, aFar);
+	out->_mat = mat4_frustum(aLeft, aRight, aBottom, aTop, aNear, aFar);
 	return out;
 }
 + (Matrix4 *)orthoWithLeft:(float)aLeft right:(float)aRight bottom:(float)aBottom top:(float)aTop near:(float)aNear far:(float)aFar {
 	Matrix4 *out = [[self alloc] init];
-	out.mat = mat4_ortho(aLeft, aRight, aBottom, aTop, aNear, aFar);
+	out->_mat = mat4_ortho(aLeft, aRight, aBottom, aTop, aNear, aFar);
 	return out;
 }
 + (Matrix4 *)lookatWithEye:(Vector3 *)aEye center:(Vector3 *)aCenter upVec:(Vector3 *)upVec {
 	Matrix4 *out = [[self alloc] init];
-	vec3_t e = aEye.vec;
-	vec3_t c = aCenter.vec;
-	vec3_t u = upVec.vec;
-	out.mat = mat4_lookat(e.x, e.y, e.z, c.x, c.y, c.z, u.x, u.y, u.z);
+	vec3_t e = aEye->_vec;
+	vec3_t c = aCenter->_vec;
+	vec3_t u = upVec->_vec;
+	out->_mat = mat4_lookat(e.x, e.y, e.z, c.x, c.y, c.z, u.x, u.y, u.z);
 	return out;
 }
 
 // Transformation matrices
 + (Matrix4 *)translationWithX:(float)x y:(float)y z:(float)z {
 	Matrix4 *out = [[self alloc] init];
-	out.mat = mat4_create_translation(x, y, z);
+	out->_mat = mat4_create_translation(x, y, z);
 	return out;
 }
 + (Matrix4 *)rotationWithAngle:(float)angle x:(float)x y:(float)y z:(float)z {
 	Matrix4 *out = [[self alloc] init];
-	out.mat = mat4_create_rotation(angle, x, y, z);
+	out->_mat = mat4_create_rotation(angle, x, y, z);
 	return out;
 }
 + (Matrix4 *)scaleWithX:(float)x y:(float)y z:(float)z {
 	Matrix4 *out = [[self alloc] init];
-	out.mat = mat4_create_scale(x, y, z);
+	out->_mat = mat4_create_scale(x, y, z);
 	return out;
 }
 
 
 - (Matrix4 *)mul:(Matrix4 *)aOther {
-	return [Matrix4 matrixWithMat:mat4_mul(_mat, aOther.mat)];
+	return [Matrix4 matrixWithMat:mat4_mul(_mat, aOther->_mat)];
 }
 - (Vector4 *)mulVector:(Vector4 *)aVector {
-	return [Vector4 vectorWithVec:vec4_mul_mat4(aVector.vec, _mat)];
+	return [Vector4 vectorWithVec:vec4_mul_mat4(aVector->_vec, _mat)];
 }
 - (Vector3 *)mulVector3:(Vector3 *)aVector isPoint:(BOOL)aIsPoint {
-	return [Vector3 vectorWithVec:vec3_mul_mat4(aVector.vec, _mat, aIsPoint)];
+	return [Vector3 vectorWithVec:vec3_mul_mat4(aVector->_vec, _mat, aIsPoint)];
 }
 - (Matrix4 *)inverse {
 	return [Matrix4 matrixWithMat:mat4_inverse(_mat, NULL)];
@@ -349,7 +349,7 @@
 	matrix_stack_push(_stack);
 }
 - (void)push:(Matrix4 *)aMat {
-	matrix_stack_push_item(_stack, aMat.mat);
+	matrix_stack_push_item(_stack, aMat->_mat);
 }
 - (void)pop {
 	matrix_stack_pop(_stack);
@@ -362,7 +362,7 @@
 	return [Matrix3 matrixWithMat:matrix_stack_get_mat3(_stack)];
 }
 - (void)mul:(Matrix4 *)aMat {
-	matrix_stack_mul_mat4(_stack, aMat.mat);
+	matrix_stack_mul_mat4(_stack, aMat->_mat);
 }
 - (void)translateWithX:(float)x y:(float)y z:(float)z {
 	matrix_stack_translate(_stack, x, y, z);
@@ -379,22 +379,22 @@
 @synthesize quat=_quat;
 + (Quaternion *)quaternionWithAngle:(float)aAngle x:(float)aX y:(float)aY z:(float)aZ {
 	Quaternion *out = [[self alloc] init];
-	out.quat = quat_createf(aAngle, aX, aY, aZ);
+	out->_quat = quat_createf(aAngle, aX, aY, aZ);
 	return out;
 }
 + (Quaternion *)quaternionWithQuat:(quat_t)aQuat {
 	Quaternion *out = [[self alloc] init];
-	out.quat = aQuat;
+	out->_quat = aQuat;
 	return out;
 }
 + (Quaternion *)quaternionFromMatrix4:(Matrix4 *)aMat {
 	Quaternion *out = [[self alloc] init];
-	out.quat = ortho_to_quat(aMat.mat);
+	out->_quat = ortho_to_quat(aMat->_mat);
 	return out;
 }
 + (Quaternion *)quaternionFromOrtho:(Matrix4 *)aMat {
 	Quaternion *out = [[self alloc] init];
-	out.quat = mat4_to_quat(aMat.mat);
+	out->_quat = mat4_to_quat(aMat->_mat);
 	return out;
 }
 
@@ -418,12 +418,12 @@
 	return [Quaternion quaternionWithQuat:quat_computeW(_quat)];
 }
 - (Vector4 *)rotatePoint:(Vector4 *)aPoint {
-	return [Vector4 vectorWithVec:quat_rotatePoint(_quat, aPoint.vec)];
+	return [Vector4 vectorWithVec:quat_rotatePoint(_quat, aPoint->_vec)];
 }
 - (float)dot:(Quaternion *)aOther {
-	return quat_dotProduct(_quat, aOther.quat);
+	return quat_dotProduct(_quat, aOther->_quat);
 }
 - (Quaternion *)slerpWithDest:(Quaternion *)aDest t:(float)aT {
-	return [Quaternion quaternionWithQuat:quat_slerp(_quat, aDest.quat, aT)];
+	return [Quaternion quaternionWithQuat:quat_slerp(_quat, aDest->_quat, aT)];
 }
 @end
