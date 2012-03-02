bf812ffbb47623d6acce0b3d581d740b44fc70fa
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-03-03 00:37:52 +0900

    Added typeerrors so that failure would not be silent

--- Tranquil/Plugins/Graphics/Scripts/load.rb
+++ Tranquil/Plugins/Graphics/Scripts/load.rb
@@ -11,8 +11,10 @@ def drawSphere(radius=1, stacks=10, slices=10)
 	scene.addImmediateModeObject Sphere.alloc.initWithRadius(radius, stacks:stacks, slices:slices)
 end
 def buildPlane(subdivs=vec2(4,4))
+    raise TypeError unless subdivs.is_a?(Vector2)
 	scene.addObject Plane.alloc.initWithSubdivisions(subdivs)
 end
 def drawPlane(subdivs=vec2(4,4))
+    raise TypeError unless subdivs.is_a?(Vector2)
 	scene.addImmediateModeObject Plane.alloc.initWithSubdivisions(subdivs)
 end
