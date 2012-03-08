e2f1b889ac18e92778b4e352012e9d999ddf1935
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-03-09 00:30:27 +0900

    Cleanup

--- Tranquil/Application/main.m
+++ Tranquil/Application/main.m
@@ -1,13 +1,3 @@
-//
-//  main.m
-//  Transitive
-//
-//  Created by Fjölnir Ásgeirsson on 1/7/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
-
 int main(int argc, char *argv[])
 {
 	return NSApplicationMain(argc, (const char **)argv);
